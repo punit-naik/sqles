b4dfdc79e24c3a8db64c89acb34f6196560dc19c
Punit Naik <naik.punit44@gmail.com>
2021-10-09 04:26:57 +0530

    added `run-query` fn and tests

--- src/org/clojars/punit_naik/sqles/core.clj
+++ src/org/clojars/punit_naik/sqles/core.clj
@@ -1,6 +1,46 @@
-(ns org.clojars.punit-naik.sqles.core)
+(ns org.clojars.punit-naik.sqles.core
+  (:require [cheshire.core :as json]
+            [clj-http.client :as http]))
 
-;; Here is where the query execution code will go
+(defmulti method->http-fn
+  (fn [method] method))
+
+(defmethod method->http-fn
+  :post
+  [_]
+  http/post)
+
+(defmethod method->http-fn
+  :get
+  [_]
+  http/get)
+
+(defmethod method->http-fn
+  :delete
+  [_]
+  http/delete)
+
+(defmethod method->http-fn
+  :put
+  [_]
+  http/put)
+
+(defn run-query
+  [{:keys [url body method query-params]
+    :or {method :get
+         query-params {}}}]
+  (when (seq url)
+    (let [post? (= method :post)
+          get? (= method :get)
+          {response :body status :status}
+          ((method->http-fn method)
+           url
+           (cond-> {:accept :json}
+             post? (merge {:content-type :json
+                           :body (json/generate-string body)})
+             get? (merge {:query-params query-params})))]
+      {:body (json/parse-string response true)
+       :status status})))
 
 (defn -main
   [& args]
--- test/org/clojars/punit_naik/sqles/core_test.clj
+++ test/org/clojars/punit_naik/sqles/core_test.clj
@@ -1,6 +1,15 @@
 (ns org.clojars.punit-naik.sqles.core-test
-  (:require [clojure.test :refer [deftest testing is]]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [org.clojars.punit-naik.sqles.config :as config]
+            [org.clojars.punit-naik.sqles.core :as core]))
 
 (deftest init-test
   (testing "Init test, passing"
-    (is (= 0 0))))
+    (let [{:keys [status]
+           {:keys [tagline]
+            {:keys [number]} :version} :body}
+          (core/run-query {:url (config/es-server)})]
+      (is (pos-int? status))
+      (if (= status 200)
+        (is (re-matches #"\d+\.\d+\.\d+" number))
+        (is (= tagline "You Know, for Search"))))))
