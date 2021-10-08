c0a746a4e6299f1994ad241db59ad0f3a0de9abd
Punit Naik <naik.punit44@gmail.com>
2021-10-09 05:07:11 +0530

    added try catch

--- src/org/clojars/punit_naik/sqles/core.clj
+++ src/org/clojars/punit_naik/sqles/core.clj
@@ -1,6 +1,11 @@
 (ns org.clojars.punit-naik.sqles.core
   (:require [cheshire.core :as json]
-            [clj-http.client :as http]))
+            [clj-http.client :as http])
+  (:import [clojure.lang ExceptionInfo]
+           [java.lang IllegalArgumentException]
+           [java.net UnknownHostException]
+           [org.apache.http ProtocolException]
+           [java.net ConnectException]))
 
 (defmulti method->http-fn
   (fn [method] method))
@@ -30,17 +35,26 @@
     :or {method :get
          query-params {}}}]
   (when (seq url)
-    (let [post? (= method :post)
-          get? (= method :get)
-          {response :body status :status}
-          ((method->http-fn method)
-           url
-           (cond-> {:accept :json}
-             post? (merge {:content-type :json
-                           :body (json/generate-string body)})
-             get? (merge {:query-params query-params})))]
-      {:body (json/parse-string response true)
-       :status status})))
+    (try
+      (let [post? (= method :post)
+            get? (= method :get)
+            {response :body status :status}
+            ((method->http-fn method)
+             url
+             (cond-> {:accept :json}
+               post? (merge {:content-type :json
+                             :body (json/generate-string body)})
+               get? (merge {:query-params query-params})))]
+        {:body (json/parse-string response true)
+         :status status})
+      (catch ExceptionInfo e
+        (let [{ex-status :status ex-body :body} (ex-data e)]
+          {:status ex-status
+           :body (json/parse-string ex-body true)}))
+      (catch UnknownHostException _ {})
+      (catch ProtocolException _ {})
+      (catch ConnectException _ {})
+      (catch IllegalArgumentException _ {}))))
 
 (defn -main
   [& args]
