98a9063f0af54c5b0de0dc37f5781cba85e26563
Punit Naik <naik.punit44@gmail.com>
2021-10-09 05:12:17 +0530

    fixed tests

--- test/org/clojars/punit_naik/sqles/core_test.clj
+++ test/org/clojars/punit_naik/sqles/core_test.clj
@@ -10,11 +10,11 @@
             {:keys [number]} :version} :body}
           (core/run-query {:url (config/es-server)})]
       (is (pos-int? status))
-      (if (and (seq status) (= status 200))
+      (if (and (not (nil? status)) (= status 200))
         (is (re-matches #"\d+\.\d+\.\d+" number))
         (is (= tagline "You Know, for Search"))))
     (let [{:keys [status] {error-msg :error} :body} (core/run-query {:url "http://localhost:9200" :method :post})]
-      (if (and (seq status) (= status 405))
+      (if (and (not (nil? status)) (= status 405))
         (is (= status 405))
         (is (= error-msg "Incorrect HTTP method for uri [/] and method [POST], allowed: [GET, HEAD, DELETE]"))))
     (is (= (core/run-query {:url "http://xyz.pqr"}) {}))
